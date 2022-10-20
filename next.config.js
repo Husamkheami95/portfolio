f83351d4e1d050e3eadd361e9787fe2fe0560e7f
Husamkheami95 <mh.hussam.khyami@gmail.com>
2022-10-20 12:37:50 +0300

    v7

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const { i18n } = require("./next-i18next.config");
-
+const basePath='.'
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   i18n,
 
-  image: {
-    verboseStackTrace: true
-}
+  images: {
+   
+    path: `${basePath}/_next/image`,
+  }
 }
 
 
