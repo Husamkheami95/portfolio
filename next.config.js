97d8b439721b57aa5435d7533987a4ca1d2b1166
Husamkheami95 <mh.hussam.khyami@gmail.com>
2022-10-20 12:26:39 +0300

    v6

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 const { i18n } = require("./next-i18next.config");
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   i18n,
+
+  image: {
+    verboseStackTrace: true
 }
+}
+
 
 module.exports = nextConfig
